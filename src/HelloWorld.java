8d8c4d9d7531234e6d541055fc940cd9713f72a6
Sofia Salome Cespedes Arze <mariasoft@gmail.com>
2022-02-26 18:06:24 -0400

    Adding Intellij.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,11 +1,9 @@
+import java.io.*;
 public class HelloWorld {
     public static void main(String[] args){
 
-
-
-
-
-        System.out.printl("Hello World");
+        System.out.println("Hello World");
+        System.out.println("More changes");
     }
 }         
 
