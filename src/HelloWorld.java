a940afa8e78c10848b1c1161fb75ab8f92ded470
Sofia Salome Cespedes Arze <mariasoft@gmail.com>
2022-02-26 23:09:46 -0400

    Spaces are added to the HelloWorld file

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,11 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args){
 
-
-
-
-
-        System.out.printl("Hello World");
+        System.out.println("Hello World");
     }
 }         
 
