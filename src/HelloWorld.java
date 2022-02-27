de0dad30d2e8b75a997117d87a0b7dc20da1d57d
sofisoftfix <34861021+sofisoftfix@users.noreply.github.com>
2022-02-27 18:47:26 -0400

    Update HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
     public static void main(String[] args){
 
         System.out.println("Hello World");
-        System.out.println("More changes");
+        System.out.println("More changes from user B"); ////changes from user B
     }
 }         
 
