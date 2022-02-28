04af191477f2fc907adc355a9324e47fc1a83995
Sofia Salome Cespedes Arze <mariasoft@gmail.com>
2022-02-27 23:25:29 -0400

    Changes from Developer 5

--- src/DiscountCalculationService.java
+++ src/DiscountCalculationService.java
@@ -1,4 +1,4 @@
 /////NEw Comments
 /////NEW COMMENTS 2/////////////////////////////////////////////////////
 ///Another change of developer 3
-///More changes from developer 3
+///More changes from developer 5
